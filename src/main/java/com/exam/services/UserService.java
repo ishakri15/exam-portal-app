488787b3db44ddefcdd827a2871fbfb1572205eb
itsdev04 <imkrdev@gmail.com>
2025-08-22 22:39:22 +0530

    created user service and web service

---
+++ src/main/java/com/exam/services/UserService.java
@@ -0,0 +1,18 @@
+package com.exam.services;
+
+import com.exam.entity.User;
+import com.exam.entity.UserRole;
+
+import java.util.Set;
+
+public interface UserService {
+
+    //creating user
+    public User createUser(User user, Set<UserRole> userRoles) throws Exception;
+
+    //get user by username
+    public User getUser(String username);
+
+    //delete user by Id
+    public void deleteUser(Long userId);
+}
